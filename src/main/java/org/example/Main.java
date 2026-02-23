7de564d0939b2a0adfd9406fb0abe6e126666ef3
Roman <85564248+Roman-S-OS@users.noreply.github.com>
2026-02-23 10:14:48 +0100

    refactor: clean up Main.java and fix formatting inconsistencies

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,20 +11,21 @@ import java.util.Objects;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Chess Patterns");
+
         String iconName = "P-B.png";
         try {
             frame.setIconImage(javax.imageio.ImageIO.read(Objects.requireNonNull(ClassLoader.getSystemResourceAsStream(iconName))));
         } catch (IOException e) {
             throw new IllegalStateException("Failed to load icon", e);
         }
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        
+
+
         frame.setLayout(new GridBagLayout());
-        frame.add(new Board() );
-        frame.setMinimumSize(new Dimension(1000, 1000));
+        frame.add(new Board());
+
         frame.pack();
-        
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
